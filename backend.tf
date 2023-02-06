2d20982a4aee36608a035155abb2a537954f5ede
USMANU ACHU <BallerGarrete>
2023-02-06 11:41:13 -0500

    Theo2

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "s3" {
-    bucket = "state-bucket-baller"
-    key    = "terraform.tfstate"
-    region = "us-west-1"
-    dynamodb_table = "state-lock"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "state-bucket-baller"
+#     key    = "terraform.tfstate"
+#     region = "us-west-1"
+#     dynamodb_table = "state-lock"
+#   }
+# }
